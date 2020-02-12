29c6391efff37a82d2ddbfd8c2154eb788a4247a
andres801 <58822692+andres801@users.noreply.github.com>
2020-02-11 23:44:52 -0600

    Add files via upload
    improving it
    example of recommended txt file:
    pedro,juan,jose,raul
    daniel,luis,octavio,roberto
    maria,janeth,miriam,arizbeth
    pamela,clara,emma,carla

---
+++ LectorDeTxt/LectorDeTxt/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace LectorDeTxt
+{
+     partial class Form1
+     {
+          /// <summary>
+          /// Variable del diseñador necesaria.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// Limpiar los recursos que se estén usando.
+          /// </summary>
+          /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Código generado por el Diseñador de Windows Forms
+
+          /// <summary>
+          /// Método necesario para admitir el Diseñador. No se puede modificar
+          /// el contenido de este método con el editor de código.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.listView1 = new System.Windows.Forms.ListView();
+               this.btn1 = new System.Windows.Forms.Button();
+               this.SuspendLayout();
+               // 
+               // listView1
+               // 
+               this.listView1.HideSelection = false;
+               this.listView1.Location = new System.Drawing.Point(13, 13);
+               this.listView1.Name = "listView1";
+               this.listView1.Size = new System.Drawing.Size(662, 139);
+               this.listView1.TabIndex = 0;
+               this.listView1.UseCompatibleStateImageBehavior = false;
+               this.listView1.View = System.Windows.Forms.View.Details;
+               // 
+               // btn1
+               // 
+               this.btn1.Location = new System.Drawing.Point(13, 158);
+               this.btn1.Name = "btn1";
+               this.btn1.Size = new System.Drawing.Size(75, 23);
+               this.btn1.TabIndex = 2;
+               this.btn1.Text = "Leer archivo";
+               this.btn1.UseVisualStyleBackColor = true;
+               this.btn1.Click += new System.EventHandler(this.Btn1_Click);
+               // 
+               // Form1
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(687, 265);
+               this.Controls.Add(this.btn1);
+               this.Controls.Add(this.listView1);
+               this.Name = "Form1";
+               this.Text = "Form1";
+               this.ResumeLayout(false);
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.ListView listView1;
+          private System.Windows.Forms.Button btn1;
+     }
+}
+
---
+++ LectorDeTxt/LectorDeTxt/Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace LectorDeTxt
+{
+     public partial class Form1 : Form
+     {
+          public Form1()
+          {
+               InitializeComponent();
+          }
+
+          private void Btn1_Click(object sender, EventArgs e)
+          {
+               try
+               {
+                    OpenFileDialog dialogo = new OpenFileDialog();
+                    if (dialogo.ShowDialog() == DialogResult.OK)
+                    {
+                         string ruta;
+                         ruta = dialogo.FileName;
+                         // Create an instance of StreamReader to read from a file.
+                         // The using statement also closes the StreamReader.
+                         using (StreamReader sr = new StreamReader(ruta))
+                         {
+                              string line;
+                              // Read and display lines from the file until the end of 
+                              // the file is reached.
+                              bool d = true;
+                              while ((line = sr.ReadLine()) != null)
+                              {
+                                   string c = "columna";
+                                   Console.WriteLine(line);
+                                   
+                                   string [] b = line.Split(',');
+                                   if (d == true)
+                                   {
+                                        for (int i = 0; i < b.Length; i++)
+                                        {
+                                             c = c + " " + (i + 1);
+                                             listView1.Columns.Add(c, 90);
+
+                                             c = "columna";
+                                        }
+                                   }
+                                   ListViewItem f = new ListViewItem(b[0]);
+                                   f.Name = b[0];
+                                   
+
+                                   for (int i = 1; i < b.Length; i++)
+                                   {
+                                        f.SubItems.Add(b[i]);
+                                   }
+                                   listView1.Items.Add(f);
+
+                                   d = false;
+
+
+                              }
+                         }
+
+                    }
+                    
+               }
+               catch (Exception a)
+               {
+                    // Let the user know what went wrong.
+                    Console.WriteLine("The file could not be read:");
+                    Console.WriteLine(a.Message);
+               }
+          }
+
+          private void Button1_Click(object sender, EventArgs e)
+          {
+               
+               string c = "columna";
+               listView1.Columns.Add("columna", 500,HorizontalAlignment.Left);
+               listView1.Columns.Add(c, 500);
+          }
+
+          private void Button1_Click_1(object sender, EventArgs e)
+          {
+
+          }
+     }
+}
